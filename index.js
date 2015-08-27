469ea46ae953a1fdcd5cc7f0ceaeeb9a00a1262b
austburn <austburn@gmail.com>
2015-08-27 13:33:02 -0400

    Add arc.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var Rectangle, Circle, Path, Michaelangelo;
+var Rectangle, Circle, Path, Ellipse, Arc, Michaelangelo;
 
 Rectangle = require('./lib/rectangle');
 Circle = require('./lib/circle');
 Path = require('./lib/path');
+Ellipse = require('./lib/ellipse');
+Arc = require('./lib/arc');
 
 Michaelangelo = {};
 Michaelangelo.Rectangle = Rectangle;
 Michaelangelo.Circle = Circle;
 Michaelangelo.Path = Path;
+Michaelangelo.Ellipse = Ellipse;
+Michaelangelo.Arc = Arc;
 
 module.exports = Michaelangelo;
---
+++ test/testArc.js
@@ -0,0 +1,24 @@
+var Arc, assert;
+
+assert = require('assert');
+Arc = require('../lib/arc');
+
+describe('arc', function() {
+    it('checkAttributes does not throw exception for accepted attributes', function () {
+        var arc;
+        arc = new Arc({x: 2, y: 2, width: 3, height: 1});
+        assert.doesNotThrow(function () {
+            arc.checkAttributes();
+        });
+    });
+
+    it('toRaphaelObject adds type and calculates path', function () {
+        var arc, raphaelObj;
+        arc = new Arc({x: 2, y: 2, width: 2, height: 2});
+        raphaelObj = arc.toRaphaelObject();
+        assert.deepEqual(raphaelObj, {
+            type: 'path',
+            path: 'M1,2A1,1 0 0,1 3,2A1,1 0 0,1 1,2'
+        });
+    });
+});
