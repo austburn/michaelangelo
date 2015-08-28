4d1470f8097d6e669d7f9bc070374e0bfd7d1869
austburn <austburn@gmail.com>
2015-08-28 15:36:53 -0400

    Allow for optional attributes.

--- test/testBaseElement.js
+++ test/testBaseElement.js
@@ -23,16 +23,6 @@ describe('baseElement', function () {
         }, /Must define a type\./);
     });
 
-    it('checkAttributes throws exception if there are the wrong number of attributes', function () {
-        var element;
-        element = new BaseElement({attribute: 'attribute1'});
-        element.type = 'element';
-
-        assert.throws(function () {
-            element.checkAttributes();
-        }, /Element does not have the expected number of keys\./);
-    });
-
     it('checkAttributes throws exception if there are invalid attributes', function () {
         var element;
         element = new BaseElement({x: 0, y: 0, bad: 1});
@@ -41,7 +31,66 @@ describe('baseElement', function () {
 
         assert.throws(function () {
             element.checkAttributes();
-        }, /Unexpected attribute: 'bad'\./);
+        }, /Element did not contain expected attributes\./);
+    });
+
+    it('checkAttributes throws exception if there are invalid attributes', function () {
+        var element;
+        element = new BaseElement({x: 0, y: 0, bad: 1});
+        element.type = 'element';
+        element.elementKeys = ['x', 'y'];
+
+        assert.throws(function () {
+            element.checkAttributes();
+        }, /Unexpected attribute: \'bad\'\./);
+    });
+
+    it('checkAttributes allows optional attributes', function () {
+        var element;
+        element = new BaseElement({x: 0, y: 0, height: 1});
+        element.type = 'element';
+        element.elementKeys = ['x', 'y'];
+        element.optionalKeys = ['height'];
+
+        assert.doesNotThrow(function () {
+            element.checkAttributes();
+        });
+    });
+
+    it('checkAttributes allows one of many optional attributes', function () {
+        var element;
+        element = new BaseElement({x: 0, y: 0, height: 1});
+        element.type = 'element';
+        element.elementKeys = ['x', 'y'];
+        element.optionalKeys = ['height', 'width', 'z'];
+
+        assert.doesNotThrow(function () {
+            element.checkAttributes();
+        });
+    });
+
+    it('checkAttributes does not allow excessive optional attributes', function () {
+        var element;
+        element = new BaseElement({x: 0, y: 0, height: 1, width: 4});
+        element.type = 'element';
+        element.elementKeys = ['x', 'y'];
+        element.optionalKeys = ['height'];
+
+        assert.throws(function () {
+            element.checkAttributes();
+        }, /Unexpected attribute: \'width\'\./);
+    });
+
+    it('checkAttributes does not allow excessive optional attributes', function () {
+        var element;
+        element = new BaseElement({x: 0, y: 0, height: 1, width: 4});
+        element.type = 'element';
+        element.elementKeys = ['x', 'y'];
+        element.optionalKeys = ['height', 'angle1', 'angle2'];
+
+        assert.throws(function () {
+            element.checkAttributes();
+        }, /Unexpected attribute: \'width\'\./);
     });
 
     it('toRaphaelObject adds options', function () {
@@ -93,6 +142,24 @@ describe('baseElement', function () {
             x: 2,
             y: 4,
             width: 6,
+            'stroke-width': 2
+        });
+    });
+
+    it('toRaphaelObject adds optional attributes', function () {
+        var element, raphaelObj;
+        element = new BaseElement({x: 0, y: 0, width: 1, height: 3}, {'stroke-width': 2});
+        element.type = 'element';
+        element.elementKeys = ['x', 'y', 'width'];
+        element.optionalKeys = ['height'];
+        raphaelObj = element.toRaphaelObject();
+
+        assert.deepEqual(raphaelObj, {
+            type: 'element',
+            x: 0,
+            y: 0,
+            width: 1,
+            height: 3,
             'stroke-width': 2
         });
     });
